d7d26284eb56fee6d74ad01fa93d1b7f46148d97
Szőke Szabolcs <szabolcsx95@gmail.com>
2016-06-24 21:48:12 +0300

    Testing lambdas

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <algorithm>
 
-int main()
+int main(int argc, char* argv[])
 {
 	// This comment was added in Visual Studio 2015
+
+	std::cout << "Invoked using args:\n";
+
+	// Printing the command line arguments
+	std::for_each(argv, argv + argc, [](char* arg)
+	{
+		std::cout << arg << std::endl;
+	});
+
 	std::cout << "Hello world!" << std::endl;
 	return 0;
 }
